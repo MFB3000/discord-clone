9f165975ee711f4fe1f843d4a8cf347726bd2c9d
Max Bernasch <max.bernasch@gmail.com>
2024-03-09 12:39:00 +0100

    Invite functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,16 +2,18 @@
 
 import { CreateServerModal } from "@/components/modal/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modal/invite-modal";
 
 export const ModalProvider = () => {
-    const [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-        setIsMounted(true)
-    }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   return (
     <>
       <CreateServerModal />
+      <InviteModal />
     </>
   );
 };
